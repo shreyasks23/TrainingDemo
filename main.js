45b4aec323b77dfcffc364029c7b09fd608e7cc3
Shreyas K S <shreyas.ks@excelindia.com>
2020-09-01 20:32:41 +0530

    this reference examples

--- main.js
+++ main.js
@@ -11,4 +11,33 @@ $(function () {
            $("#sample").text(func(a,b));
         });
     });
+
+    $("#Concatinate").on('click', function () {
+        
+        let firstName = $('#Num1').val();
+        let lastName = $('#Num2').val();
+    
+        function Student( firstName, lastName) {            
+            this.firstName = firstName;
+            this.lastName = lastName;
+            var obj = this;
+            this.fullNameUsingAnonymous = function () {
+                $("#sample3").text(this.firstName + " " + this.lastName) 
+                setTimeout(function () {
+                    //creates a new instance of this ,hides outer scope of this
+                    $("#sample1").text(obj.firstName + " " + obj.lastName)                    
+                }, 1000)
+            }
+            this.fullNameUsingArrow = function () {
+                setTimeout(() => {
+                    //uses this instance of outer scope
+                    $("#sample2").text(this.firstName + " " + this.lastName)                    
+                }, 3000)
+            }
+        }
+        var s1 = new Student(firstName, lastName);
+        s1.fullNameUsingAnonymous();
+        s1.fullNameUsingArrow();     
+        
+    });
 });
